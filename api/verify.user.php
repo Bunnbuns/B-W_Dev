1bbedacd224da0e902cc951ea38200b853d77ae5
Ben Border <bunnbunsisawesome@gmail.com>
2019-02-06 14:10:11 -0500

    Add files via upload

--- api/verify.user.php
+++ api/verify.user.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-
-//temp for dev
+#temp
 error_reporting(E_ALL); ini_set('display_errors', 1); 
 
 include('config.php');
@@ -9,28 +8,30 @@ include ('common.php');
 
 $connection = new PDO($dsn, $username, $password, $options);
 
-if(isset($_GET['email']) && $_GET['email'] !== ""){
+header("Content-Type", "application/json");
+
+if(isset($_POST['email']) && $_POST['email'] !== ""){
     //Check password by email
     $sql = "SELECT id, name, username, email, password 
                     FROM users
                     WHERE email = :email";
 
-    $email = $_GET['email'];
-    $password = $_GET['password'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
     $statement = $connection->prepare($sql);
     $statement->bindParam(':email', $email, PDO::FETCH_ASSOC);
     $statement->execute();
     $data = $statement->fetchAll(PDO::FETCH_ASSOC);
     
-}else if(isset($_GET['username']) && $_GET['username'] !== ""){
+}else if(isset($_POST['username']) && $_POST['username'] !== ""){
     //Check password by username
     $sql = "SELECT id, name, username, email, password 
                     FROM users
                     WHERE username = :username";
 
-    $username = $_GET['username'];
-    $password = $_GET['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
     $statement = $connection->prepare($sql);
     $statement->bindParam(':username', $username, PDO::FETCH_ASSOC);
@@ -40,11 +41,11 @@ if(isset($_GET['email']) && $_GET['email'] !== ""){
     //not enough post boiz
     echo "No username or email specified.\n";
 }
-if(isset($_GET['password']) && $_GET['password'] !== ""){
+if(isset($_POST['password']) && $_POST['password'] !== ""){
     if (password_verify($password, $data[0]['password'])) {
         $_SESSION['username']=$username;
         //good
-        echo $_COOKIE['PHPSESSID'];
+        echo "ok\n";
     } else {
         //bad
         echo "Invalid password.\n";
